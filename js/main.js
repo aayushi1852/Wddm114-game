7572c0083b02c20412d3dad9c35eacc0d488d08d
aayushi shah <98124989+aayushi1852@users.noreply.github.com>
2022-04-22 19:55:53 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,58 @@
+const button = document.querySelector("button");
+const holes = document.querySelectorAll(".hole");
+const moles = document.querySelectorAll(".mole");
+const scoreBoard = document.querySelector(".score");
+let score = 0;
+let timeIsUp = false;
+let lastHole;
+
+function randomTime(min,max){
+    return Math.round(Math.random() * (max-min) + min);
+}
+function randomHole(holes){
+    const index =  Math.floor(Math.random() * holes.length);
+    const hole = holes[index];
+    if (hole === lastHole){
+        return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
+function appear() {
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+    console.log(hole);
+    hole.classList.add("up");
+    
+    setTimeout(() => {
+    
+        hole.classList.remove("up");
+
+        if(!timeIsUp) {
+            appear();
+        }
+
+    }, time);
+}
+   
+
+function startGame() {
+    scoreBoard.textContent = 0;
+    timeIsUp = false;
+    score = 0;
+    const timeout = 10000;
+
+    appear();
+    //END the game when time is up
+    setTimeout(() =>
+        (timeIsUp = true), timeout);   
+}
+
+
+function whack() {
+    score++;
+    this.classList.remove("up");
+    scoreBoard.textContent = score;
+}
+button.addEventListener("click",startGame);
+moles.forEach((mole) => mole.addEventListener("click", whack));
